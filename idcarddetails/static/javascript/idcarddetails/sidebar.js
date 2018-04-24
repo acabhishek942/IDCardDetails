dde0585f59ac13c9cfb13ef91fecd728a3046e02
Abhishek Choudhary <acabhishek942@gmail.com>
2018-04-24 22:46:46 +0530

    added sidebar for navigating between different id cards

---
+++ idcarddetails/static/javascript/idcarddetails/sidebar.js
@@ -0,0 +1,8 @@
+function toggleSideBar() {
+    var sideBar = document.getElementById("mySidenav");
+    if (sideBar.style.display === "none") {
+        sideBar.style.display = "block";
+    } else {
+        sideBar.style.display = "none";
+    }
+}
