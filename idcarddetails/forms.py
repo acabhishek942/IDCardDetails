2a2bb56b5772ec45171d733379a49e39ff08dcf9
Abhishek Choudhary <acabhishek942@gmail.com>
2018-04-25 00:41:26 +0530

    stylize the idcard number form

--- idcarddetails/forms.py
+++ idcarddetails/forms.py
@@ -3,6 +3,13 @@ from django import forms
 from .models import IDCardNumbers
 
 class IDCardNumbersForm(forms.ModelForm):
+	def __init__(self, *args, **kwargs):
+		super(IDCardNumbersForm, self).__init__(*args, **kwargs)
+		for field_name in self.fields:
+		    field = self.fields.get(field_name)  
+		    if field:
+		        if type(field.widget) in (forms.TextInput, forms.DateInput):
+		            field.widget = forms.TextInput(attrs={'placeholder': field.label})
 
 	class Meta:
 		model = IDCardNumbers
