810b5daea64b3a2a8b12068cd1f1537f0b02f8dd
Abhishek Choudhary <acabhishek942@gmail.com>
2018-04-26 23:48:53 +0530

    added tiles for displaying id crad numbers

--- idcarddetails/views.py
+++ idcarddetails/views.py
@@ -4,6 +4,7 @@ from django.template import loader
 from allauth.account.views import LoginView
 from allauth.account.forms import SignupForm, LoginForm
 from .forms import IDCardNumbersForm
+from .models import IDCardNumbers
 
 
 def index(request):
@@ -17,5 +18,21 @@ class LoginAndSignUpView(LoginView):
 
 def profile(request):
 	template = loader.get_template('idcarddetails/profile.html')
-	context = {'idCardDetailsForm' : IDCardNumbersForm}
-	return HttpResponse(template.render(context, request))
+	if request.method == 'POST':
+		idcardForm = IDCardNumbersForm(request.POST)
+		if idcardForm.is_valid():
+			aadharNumber = idcardForm.cleaned_data['aadharNumber']
+			drivingLicenseNumber = idcardForm.cleaned_data['drivingLicenseNumber']
+			voterCardNumber = idcardForm.cleaned_data['voterCardNumber']
+			rationCardNumber = idcardForm.cleaned_data['rationCardNumber']
+			passportNumber = idcardForm.cleaned_data['passportNumber']
+			userCardsDetails = IDCardNumbers(request.user.id, aadharNumber, drivingLicenseNumber, 
+				voterCardNumber, rationCardNumber, passportNumber)
+			userCardsDetails.save()
+			return HttpResponse(template.render({'cardsDetails' : [userCardsDetails], 'detailsExist' : True}, request))
+	userCardDetails = IDCardNumbers.objects.filter(username=request.user.id)
+	if userCardDetails.exists():
+		return HttpResponse(template.render({'cardsDetails' : userCardDetails, 'detailsExist' : True}, request))
+	else:
+		context = {'idCardDetailsForm' : IDCardNumbersForm}
+		return HttpResponse(template.render(context, request))
