8f54fc45b970b497ede5312f5cd216f4045f928e
Abhishek Choudhary <acabhishek942@gmail.com>
2018-05-06 05:05:48 +0530

    close sidebar on clicking esc key

--- src/idcarddetails/static/javascript/idcarddetails/sidebar.js
+++ src/idcarddetails/static/javascript/idcarddetails/sidebar.js
@@ -6,3 +6,12 @@ function toggleSideBar() {
         sideBar.style.display = "none";
     }
 }
+
+$(document).keyup(function(e) {
+     if (e.keyCode == 27) { 
+     	var sideBar = document.getElementById("mySidenav");
+     	if (sideBar.style.display === 'block'){
+     		sideBar.style.display = "none";
+     	}
+    }
+});
